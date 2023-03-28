883c5d06e309f8482531c4a4bc2a1b6ad9dc6ca4
dragonechoi <vasco1379@naver.com>
2023-03-28 15:20:49 +0900

    라이브러리 추가및 오류 수정

--- app/src/main/java/com/cys/honeydog/activities/LoginActivity.kt
+++ app/src/main/java/com/cys/honeydog/activities/LoginActivity.kt
@@ -1,12 +1,27 @@
 package com.cys.honeydog.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cys.honeydog.R
+import com.cys.honeydog.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+        val binding:ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+       // setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.newId.setOnClickListener{clickId()}
+
     }
+
+    fun clickId(){
+        val intent=Intent(this,NewIdActivity::class.java)
+        startActivity(intent)
+    }
+
 }
--- app/src/main/java/com/cys/honeydog/activities/NewIdActivity.kt
+++ app/src/main/java/com/cys/honeydog/activities/NewIdActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cys.honeydog.R
 
-class NewId : AppCompatActivity() {
+class NewIdActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_id)
