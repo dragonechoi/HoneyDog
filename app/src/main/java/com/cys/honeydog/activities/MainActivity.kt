d1eacecf535a97e3083f1ca2adb11428ef37be62
dragonechoi <vasco1379@naver.com>
2023-04-24 17:40:01 +0900

    메인 Activity back press 클릭시 다이알 로그 나오고 눌러야 나가게 설정

--- app/src/main/java/com/cys/honeydog/activities/MainActivity.kt
+++ app/src/main/java/com/cys/honeydog/activities/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.cys.honeydog.activities
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.cys.honeydog.R
 import com.cys.honeydog.databinding.ActivityMainBinding
@@ -12,7 +14,7 @@ import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
-
+    private var backPressedTime: Long = 0 // 뒤로가기 버튼을 누른 시간을 저장할 변수
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,6 +25,22 @@ class MainActivity : AppCompatActivity() {
 
         initNavigationBar()
 
+    }
+    override fun onBackPressed() {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - backPressedTime < 2000) { // 2초 이내에 뒤로가기 버튼을 다시 누른 경우
+            super.onBackPressed()
+        } else {
+            backPressedTime = currentTime // 이전 시간을 현재 시간으로 대체
+            val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+            builder.setMessage("앱을 종료하시겠습니까?")
+            builder.setPositiveButton("예",
+                DialogInterface.OnClickListener { dialog, which ->
+                    finish() // 앱을 종료
+                })
+            builder.setNegativeButton("아니오", null)
+            builder.show()
+        }
     }
 
     //Main Activity Fragment 화면 전환
